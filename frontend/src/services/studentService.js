3b1a0753ff824e3ee226fd82bd48f9d55339b637
nishant-salekar <nishantsalekar1979@gmail.com>
2026-06-17 18:28:32 +0530

    made changes

--- frontend/src/services/studentService.js
+++ frontend/src/services/studentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/students";
+const API_URL = "https://student-management-system-7via.onrender.com/api/students";
 
 export const getStudents = async () => {
   const response = await axios.get(API_URL);
